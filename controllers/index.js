d11a8e5c56406d66742d7bd7d9a44f2dc2b04f32
avanim <axm1205@case.edu>
2022-12-21 18:18:16 -0500

    set up initial controllers folder structure

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// CREATE, UPDATE, DELETE routes
+const apiRoutes = require('./api');
+// GET routes (render view)
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
