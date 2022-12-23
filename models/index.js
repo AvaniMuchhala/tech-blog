fd44c7d9f6a69cc7198b936ebbf599bf7e562830
avanim <axm1205@case.edu>
2022-12-23 08:07:36 -0500

    added Blogpost and Comment models and associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,40 @@
 const User = require('./User');
+const Blogpost = require('./Blogpost');
+const Comment = require('./Comment');
+
+// User can have many blogposts (if delete user, also delete their blogposts)
+User.hasMany(Blogpost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Blogpost belongs to a User
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Blogpost can have many comments from different users
+// (if delete blogpost, also delete associated comments)
+Blogpost.hasMany(Comment, {
+    foreignKey: 'comment_id',
+    onDelete: 'CASCADE'
+});
+
+// Comments belong to a blogpost
+Comment.belongsTo(Blogpost, {
+    foreignKey: 'comment_id'
+});
+
+// User can have many comments (if delete user, delete their comments too)
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Comments belong to a User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
-module.exports = { User };
+module.exports = { User, Blogpost, Comment };
